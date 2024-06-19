a6e084da6ab3652db2aec42fc5b36c8677b77fc2
Chris Zuber <shgysk8zer0@gmail.com>
2024-06-19 11:06:33 -0700

    Load `@aegisjsproject/core/bundle.js`
    The previous polyfill didn't seem to work correctly.

--- polyfills.js
+++ polyfills.js
@@ -2,6 +2,6 @@ import '@shgysk8zer0/polyfills/object.js';
 import '@shgysk8zer0/polyfills/map.js';
 import '@shgysk8zer0/polyfills/promise.js';
 import '@shgysk8zer0/polyfills/CSSStyleSheet.js';
-import '@aegisjsproject/core/polyfill.js';
+import '@aegisjsproject/core/bundle.js';
 import '@shgysk8zer0/polyfills/elementInternals.js';
 import '@shgysk8zer0/polyfills/element.js';
