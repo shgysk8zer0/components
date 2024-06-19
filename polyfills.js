4631e7092771ebcbb9d5e4946b9103c8da955750
Chris Zuber <shgysk8zer0@gmail.com>
2024-06-19 10:39:59 -0700

    Add polyfills module & bundle
    This contains the core set of polyfills needed for most components

---
+++ polyfills.js
@@ -0,0 +1,7 @@
+import '@shgysk8zer0/polyfills/object.js';
+import '@shgysk8zer0/polyfills/map.js';
+import '@shgysk8zer0/polyfills/promise.js';
+import '@shgysk8zer0/polyfills/CSSStyleSheet.js';
+import '@aegisjsproject/core/polyfill.js';
+import '@shgysk8zer0/polyfills/elementInternals.js';
+import '@shgysk8zer0/polyfills/element.js';
